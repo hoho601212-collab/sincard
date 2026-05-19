ed5b7037a6513d5262b484b8b3c384f5e58e0c63
hoho601212-collab <hoho601212@gmail.com>
2026-05-19 12:03:15 +0900

    Update Header.tsx

--- components/design-system/Header.tsx
+++ components/design-system/Header.tsx
@@ -26,7 +26,7 @@ export const Header: React.FC<HeaderProps> = ({
   logoHref = '/',
   navItems = [
     { label: '고객센터', href: '/#notice', active: false },
-    { label: '회사소개', href: 'https://www.pin-toss.com/signup', active: false },
+    { label: '회사소개', href: '/about', active: false },
     { label: '공지사항', href: '/faq', active: false },
     { label: '자주 묻는 질문', href: '/notice', active: false },
   ],
